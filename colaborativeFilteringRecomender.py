80402890c75dc9e2a9a751c398c72ee82659564e
Haltz01 <pedro-x-guerra@hotmail.com>
2021-12-12 16:53:01 -0300

    Organizing and fixing CF method

--- colaborativeFilteringRecomender.py
+++ colaborativeFilteringRecomender.py
@@ -1,62 +1,17 @@
 import pandas as pd
 import numpy as np
+from pandas.core.frame import DataFrame
 from sklearn.neighbors import NearestNeighbors
 from scipy.sparse import csr_matrix
 
-# import anime_name dataset
-cols_to_use = ['MAL_ID', 'Name']
-anime_name_df = pd.read_csv('anime.csv', usecols = cols_to_use)
-
-display(anime_name_df.shape)
-anime_name_df.head()
-
-# Importing rating dataset
-
-rating_df = pd.read_csv('rating_complete.csv')
-
-display(rating_df.shape)
-rating_df.head()
-
-# Counting number of unique users (check user_id)
-users_count = rating_df.groupby('user_id').size().reset_index()
-users_count.columns = ["user_id", "anime_count"]
-users_count.set_index('user_id', inplace = True)
-print('Numbers of unique users : ', users_count.shape[0])
-
 MIN_ANIME_REV = 500
 
-# Filtering the users
-# Only getting the users that reviewed at least MIN_ANIME_REV animes
-filtered_users = users_count[users_count.anime_count >= MIN_ANIME_REV]
-users_ids = list(filtered_users.index)
-print('Numbers of unique users with {} or more anime ratings: {}'.format(MIN_ANIME_REV, len(users_ids)))
-
-# Getting only users that are in our users_ids list (users with >= MIN_ANIME_REV anime reviews)
-rating_df = rating_df[rating_df['user_id'].isin(users_ids)]
-
-print("Rating shape:", rating_df.shape)
-print(rating_df.info())
-
-# Vectorization
-unique_users = {int(x): i for i, x in enumerate(rating_df.user_id.unique())} # getting unique user_id from rating_df
-unique_animes = {int(x): i for i, x in enumerate(anime_name_df.MAL_ID.unique())} # getting unique animes from anime_name_df
-
-print(len(unique_animes), len(unique_users))
-
-# Creating and filling collaborative-filter matrix (maybe it's sparse, we should check and optimize memory used...)
-anime_collaborative_filter = np.zeros((len(unique_animes), len(unique_users)))
-
-# For each combination of user, anime and rating given by that user for that anime (rating)...
-for user_id, MAL_ID, rating in rating_df.values:
-  anime_collaborative_filter[unique_animes[MAL_ID]][unique_users[user_id]] = rating # ... we insert the rating in our cf matrix
-display(anime_collaborative_filter) # each line represents an anime and each column represents all ratings given by a user to all animes (0 = no rating or actually 0)
-
-def getCFRecommendation(title, n_neighbors = 10):
+def getCFRecommendation(anime_collaborative_filter: np.ndarray, anime_name_df: DataFrame, title: str, n_neighbors: int = 10):
   # Creating KNN to classify our animes based on the ratings
   model_knn = NearestNeighbors(metric='cosine', n_neighbors = n_neighbors)
   model_knn.fit(csr_matrix(anime_collaborative_filter))
   
-  query_index = anime_name_df[anime_name_df['Name']==title].index[0]
+  query_index = anime_name_df[anime_name_df['Name'].str.lower() == title.lower()].index[0]
 
   distances, indices = model_knn.kneighbors(anime_collaborative_filter[query_index,:].reshape(1, -1), n_neighbors = n_neighbors)
   
@@ -69,8 +24,95 @@ def getCFRecommendation(title, n_neighbors = 10):
         
   return pd.DataFrame(result)
 
-getCFRecommendation('Kimetsu no Yaiba')
+def createCFMatrix(unique_animes: int, unique_users: int, rating_df: DataFrame):
+  # Creating and filling collaborative-filter matrix (maybe it's sparse, we should check and optimize if possible...)
+  anime_collaborative_filter = np.zeros((len(unique_animes), len(unique_users)))
 
-getCFRecommendation('No Game No Life', 7)
+  # For each combination of user, anime and rating given by that user for that anime (rating)...
+  for user_id, MAL_ID, rating in rating_df.values:
+    anime_collaborative_filter[unique_animes[MAL_ID]][unique_users[user_id]] = rating # ... we insert the rating in our cf matrix
+  
+  print("-> CF matrix shape:", anime_collaborative_filter.shape)
+  print(anime_collaborative_filter) # each line represents an anime and each column represents all ratings given by a user to all animes (0 = no rating or actually 0)
+  
+  return anime_collaborative_filter
 
-getCFRecommendation('Kiss x Sis (TV)', 7)
+def getUniqueIdsCount(anime_name_df: DataFrame, rating_df: DataFrame):
+  # Vectorization
+  unique_users = {int(x): i for i, x in enumerate(rating_df.user_id.unique())} # getting unique user_id from rating_df
+  unique_animes = {int(x): i for i, x in enumerate(anime_name_df.MAL_ID.unique())} # getting unique anime names from anime_name_df
+
+  print("-> Qty. unique users:", len(unique_users))
+  print("-> Qty. uique animes:", len(unique_animes))
+
+  return unique_users, unique_animes
+
+
+def readAndJoinAnimeCSVs():
+  # import anime_name dataset
+  cols_to_use = ['MAL_ID', 'Name']
+  anime_name_df = pd.read_csv('dataset/anime.csv', usecols = cols_to_use)
+
+  # Showing some info about the dataset
+  print("-> Anime name dataset loaded")
+  print(anime_name_df.shape)
+  print(anime_name_df.head())
+
+  # Importing rating dataset
+  rating_df = pd.read_csv('dataset/rating_complete.csv', nrows = 900000)
+
+  # Showing some info about the dataset
+  print("-> Rating dataset loaded")
+  print(rating_df.shape)
+  print(rating_df.head())
+
+  # Counting number of unique users (check user_id)
+  users_count = rating_df.groupby('user_id').size().reset_index()
+  
+  # Defining user_id as dataframe index
+  users_count.columns = ["user_id", "anime_count"]
+  users_count.set_index('user_id', inplace = True)
+  print('-> Numbers of unique users: ', users_count.shape[0])
+
+  # Filtering the users
+  # Only getting the users that reviewed at least MIN_ANIME_REV animes
+  # Avoiding (very) sparse matrix
+  filtered_users = users_count[users_count.anime_count >= MIN_ANIME_REV]
+  users_ids = list(filtered_users.index)
+  print('-> Numbers of unique users with {} or more anime ratings: {}'.format(MIN_ANIME_REV, len(users_ids)))
+
+  # Getting only users that are in our users_ids list (users with >= MIN_ANIME_REV anime reviews)
+  rating_df = rating_df[rating_df['user_id'].isin(users_ids)]
+
+  print("-> New rating shape (with filtered users):", rating_df.shape)
+  print(rating_df.info())
+
+  return anime_name_df, rating_df
+
+def main():
+  anime_name_df, rating_df = readAndJoinAnimeCSVs()
+  unique_users, unique_animes = getUniqueIdsCount(anime_name_df, rating_df)
+  anime_collaborative_filter = createCFMatrix(unique_animes, unique_users, rating_df)
+
+  want_to_search = True
+  while (want_to_search):
+    search_anime = input("# Entre com o nome de um anime:\n>> ")
+    if (search_anime == ""):
+      continue
+
+    search_anime = search_anime.strip()
+
+    if not search_anime.lower() in anime_name_df['Name'].str.lower().tolist():
+        print("-> Título não encontrado na nossa base de dados...")
+        continue
+
+    result = getCFRecommendation(anime_collaborative_filter, anime_name_df, search_anime, n_neighbors = 10)
+    print("# Recomendações para \"{}\":\n".format(search_anime), result)
+
+    user_choise_sn = input("# Deseja buscar por outro anime? (s/n)\n>> ")
+
+    if (user_choise_sn.strip() == "n"):
+      want_to_search = False
+
+if __name__ == "__main__":
+    main()
